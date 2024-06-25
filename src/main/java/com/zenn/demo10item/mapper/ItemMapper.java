f0c57d0aadf56bf3dc2e123eb7ad749ac9f3df7c
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-06-25 14:25:16 +0900

    MyBatis（Mapper）の記述を修正

--- src/main/java/com/zenn/demo10item/mapper/ItemMapper.java
+++ src/main/java/com/zenn/demo10item/mapper/ItemMapper.java
@@ -3,7 +3,6 @@ package com.zenn.demo10item.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import com.zenn.demo10item.entity.Item;
 
@@ -11,7 +10,7 @@ import com.zenn.demo10item.entity.Item;
 public interface ItemMapper {
 	Item findById(int id);
     List<Item> findAll();
-    int insert(@Param("item") Item item);
-    int update(@Param("item") Item item);
+    int insert(Item item);
+    int update(Item item);
     boolean delete(int id);
 }
